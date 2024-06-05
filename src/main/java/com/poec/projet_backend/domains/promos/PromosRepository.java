8acdfd017d2fe8bcb2026a1d8e4ad613bd468c2a
e-n-p <nickellam0@gmail.com>
2024-06-05 11:54:18 +0200

    creation of promo entity and api collection

---
+++ src/main/java/com/poec/projet_backend/domains/promos/PromosRepository.java
@@ -0,0 +1,6 @@
+package com.poec.projet_backend.domains.promos;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PromosRepository extends JpaRepository<Promos, Long> {
+}
