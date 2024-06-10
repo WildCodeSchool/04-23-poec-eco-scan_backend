0635f26feaba9550938b773347521f9c3b0d2257
e-n-p <nickellam0@gmail.com>
2024-06-10 14:57:55 +0200

    added get user by email call

--- src/main/java/com/poec/projet_backend/domains/userImpl/UserImplController.java
+++ src/main/java/com/poec/projet_backend/domains/userImpl/UserImplController.java
@@ -3,8 +3,14 @@ package com.poec.projet_backend.domains.userImpl;
 import com.poec.projet_backend.domains.AbstractController;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/user")
@@ -15,5 +21,18 @@ public class UserImplController extends AbstractController<UserImpl> {
     @Autowired
     private UserImplService userService;
 
+<<<<<<< Updated upstream
+=======
+    @GetMapping("/get/email/{email}")
+    public ResponseEntity<UserImpl> getById(@PathVariable String email) {
+        List<UserImpl> users = userService.getAll();
+        for (UserImpl user : users){
+            if (user.getEmail().equals(email)){
+                return new ResponseEntity<>(user, HttpStatus.OK);
+            }
+        }
+        return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
+    }
+>>>>>>> Stashed changes
 
 }
