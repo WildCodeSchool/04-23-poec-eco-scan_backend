764c7efc683ad072f66e0a367c419cb14f927aad
Victor G <victorgj95@outlook.com>
2024-06-11 16:16:42 +0200

    victor le farmer

--- src/main/java/com/poec/projet_backend/domains/promos/PromosController.java
+++ src/main/java/com/poec/projet_backend/domains/promos/PromosController.java
@@ -3,15 +3,23 @@ package com.poec.projet_backend.domains.promos;
 import com.poec.projet_backend.domains.AbstractController;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/promos")
 @RequiredArgsConstructor
 public class PromosController extends AbstractController<Promos> {
 
-    @Autowired
     private PromosService promosService;
 
+    @GetMapping("/get/percentOff")
+    public ResponseEntity<List<Promos>> getPromosOrderedByPercentOff() {
+        List<Promos> promotions = promosService.sortByPercentOff();
+        return ResponseEntity.ok(promotions);
+    }
 }
--- src/main/java/com/poec/projet_backend/domains/promos/PromosRepository.java
+++ src/main/java/com/poec/projet_backend/domains/promos/PromosRepository.java
@@ -1,6 +1,9 @@
 package com.poec.projet_backend.domains.promos;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
+
 
 public interface PromosRepository extends JpaRepository<Promos, Long> {
+    List<Promos> sortPromosByPercentOff();
 }
