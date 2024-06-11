a66b73ef6fd055aa5fadf145182b325fb6e2059c
e-n-p <nickellam0@gmail.com>
2024-06-11 16:54:25 +0200

    unused dependencies

--- src/main/java/com/poec/projet_backend/domains/rubbish/RubbishController.java
+++ src/main/java/com/poec/projet_backend/domains/rubbish/RubbishController.java
@@ -1,12 +1,8 @@
 package com.poec.projet_backend.domains.rubbish;
 
 import com.poec.projet_backend.domains.AbstractController;
-import com.poec.projet_backend.domains.type.TypeMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
